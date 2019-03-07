b4a3dcad36470b0892affa239be5e5bfa1bc1292
PRAKHAR AGARWAL <500068993@DDN.UPES.AC.IN>
2019-03-07 10:07:26 +0530

    First commit on c++ programm hello.cpp withoout definition of member functions

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>  
+using namespace std;  
+class Student {  
+   public:  
+       int id;     
+       string name;      
+       void insert(int i, string n) 
+       void display()       
+};  
+int main() 
+{  
+    Student s1;    
+    Student s2;   
+    s1.insert(201, "Sonoo");    
+    s2.insert(202, "Nakul");    
+    s1.display();    
+    s2.display();  
+    return 0;  
+}  

2d875b99617f656e1e8d4788882c8420aa5d55e5
PRAKHAR AGARWAL <500068993@DDN.UPES.AC.IN>
2019-03-07 10:45:33 +0530

    Last commit

--- hello.cpp
+++ hello.cpp
@@ -11,7 +11,7 @@ int main()
 {  
     Student s1;    
     Student s2;   
-    s1.insert(201, "Sonoo");    
+    s1.insert(201, "SSSSSSSSonoo");    
     s2.insert(202, "Nakul");    
     s1.display();    
     s2.display();  
